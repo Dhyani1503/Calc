89433a6a40ed0d06e5bae0a0179b1945b6659922
Dhyani1503 <88771561+Dhyani1503@users.noreply.github.com>
2022-09-20 12:33:38 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,18 @@
+const sum = (a,b) =>
+{
+    return a+b;
+}
+const sub = (a,b) =>
+{
+    return b-a;
+}
+const mul = (a,b) =>
+{
+    return a*b;
+}
+const mod = (a,b) =>
+{
+    return a%b;
+}
+
+module.exports = {sum,sub,mul,mod}
---
+++ index.js
@@ -0,0 +1,10 @@
+const calculator = require('./calc');
+
+let r  = calculator.sum(2,3)
+console.log(r)
+let s  = calculator.sub(2,3)
+console.log(s)
+let t  = calculator.mul(2,3)
+console.log(t)
+let u  = calculator.mod(2,3)
+console.log(u)
